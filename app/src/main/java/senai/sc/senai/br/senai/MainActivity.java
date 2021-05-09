dccb114bd24671bc87501edae20b08ce2f6778ea
cmcleon <77648947+cmcleon@users.noreply.github.com>
2021-05-09 14:38:59 -0300

    :package: atividade 1 - poo

--- app/src/main/java/senai/sc/senai/br/senai/MainActivity.java
+++ app/src/main/java/senai/sc/senai/br/senai/MainActivity.java
@@ -8,18 +8,28 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.et_texto);
+        editText2 = findViewById(R.id.et_texto2);
 
     }
 
     public void onClickBtnMostrar(View v){
         EditText editText = findViewById(R.id.et_texto);
-        Toast.makeText(MainActivity.this, editText.getText(), Toast.LENGTH_LONG).show();
+        EditText editText2 = findViewById(R.id.et_texto2);
+        Toast.makeText(MainActivity.this, editText.getText().toString() + " "+ editText2.getText().toString(), Toast.LENGTH_LONG).show();
 
+    }
+
+    public void onClickBtnLimpar(View v){
+        editText.setText("");
+        editText2.setText("");
     }
 
 }
